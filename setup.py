ac598046b7293e7dfe0a45f042e5a4bcaf3a0edf
s175548 <s175548@student.dtu.dk>
2023-10-02 09:45:34 +0200

    Made data cleaner for ONEIL

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup the package."""
+
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="graph_package",
+    version="0.1.0",
+    license="Apache License, Version 2.0",
+    description="A Deep Learning Library for Drug Pair Scoring.",
+    author="Benedek Rozemberczki and Charles Hoyt",
+    author_email="benedek.rozemberczki@gmail.com",
+)
