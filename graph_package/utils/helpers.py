8d68fb86ca1235c3d57d888897c21aaae6622b50
s175548 <s175548@student.dtu.dk>
2023-10-05 14:42:05 +0200

    made gold table for oneil

--- graph_package/utils/helpers.py
+++ graph_package/utils/helpers.py
@@ -1,6 +1,8 @@
 import logging
+import colorlog
 from pathlib import Path
 from graph_package.configs.directories import Directories
+import sys
 
 def init_logger():
     # Create a logger object
@@ -11,15 +13,24 @@ def init_logger():
     logger.setLevel(logging.DEBUG)
 
     # Create a file handler
-    handler = logging.FileHandler(log_file)
-    handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
 
-    # Create a logging format
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-
-    # Add the handler to the logger
-    logger.addHandler(handler)
+    # Create a stream handler with colored output
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(colorlog.ColoredFormatter(
+        '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        }))
+    
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
 
     return logger
-
