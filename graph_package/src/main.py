57eb2ccfef988ba790a720070c9977e0f0aa77ee
s175548 <s175548@student.dtu.dk>
2023-10-16 18:15:42 +0200

    ...

--- graph_package/src/main.py
+++ graph_package/src/main.py
@@ -1,27 +1,20 @@
 """main module."""
 
-from typing import List, Tuple, Union
-from pytorch_lightning.loggers import WandbLogger
+from typing import List, Tuple
 from pytorch_lightning.loggers import WandbLogger
 from models import RESCAL
 from graph_package.configs.definitions import model_dict, dataset_dict
-from graph_package.configs.definitions import model_dict, dataset_dict
 from graph_package.configs.directories import Directories
-from torch.utils.data import random_split, Subset
-from models import DeepDDS, RESCAL
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from torch.utils.data import Subset
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 import hydra
 from dotenv import load_dotenv
 from sklearn.model_selection import KFold, train_test_split as train_val_split
-from sklearn.model_selection import KFold, train_test_split as train_val_split
-from graph_package.utils.meter import WandbMeter
-from torchdrug.data import DataLoader
 from torchdrug.data import DataLoader
 import os
 from pytorch_lightning import Trainer
 import sys
-import wandb 
+import wandb
 import shutil
 
 
@@ -38,7 +31,7 @@ def reset_wandb_env():
 
 def load_data(model: str = "deepdds", dataset: str = "oneil"):
     """Fetch formatted data depending on modelling task"""
-    dataset_key =dataset.lower()+ "_" + model.lower() 
+    dataset_key = dataset.lower() + "_" + model.lower()
     dataset = dataset_dict[dataset_key]()
     return dataset
 
@@ -48,16 +41,19 @@ def init_model(model: str = "deepdds", model_kwargs: dict = {}):
     model = model_dict[model.lower()](**model_kwargs)
     return model
 
+
 def get_model_name(config: dict, sys_args: List[str]):
     for arg in sys_args:
         if arg.startswith("model="):
             return arg.split("=")[1]
     return "deepdds"
 
-def get_checkpoint_path(model_name: str,k: int):
+
+def get_checkpoint_path(model_name: str, k: int):
     checkpoint_path = Directories.CHECKPOINT_PATH / model_name / f"fold_{k}"
     checkpoint_path.mkdir(parents=True, exist_ok=True)
     return str(checkpoint_path)
+
 
 def get_dataloaders(datasets: List[DataLoader], batch_size: int):
     dataloaders = []
@@ -65,10 +61,13 @@ def get_dataloaders(datasets: List[DataLoader], batch_size: int):
         dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=0)
         dataloaders.append(dataloader)
     return dataloaders
-def get_checkpoint_path(model_name: str,k: int):
+
+
+def get_checkpoint_path(model_name: str, k: int):
     checkpoint_path = Directories.CHECKPOINT_PATH / model_name / f"fold_{k}"
     checkpoint_path.mkdir(parents=True, exist_ok=True)
     return str(checkpoint_path)
+
 
 def get_dataloaders(datasets: List[DataLoader], batch_size: int):
     dataloaders = []
@@ -76,6 +75,7 @@ def get_dataloaders(datasets: List[DataLoader], batch_size: int):
         dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=0)
         dataloaders.append(dataloader)
     return dataloaders
+
 
 def split_dataset(
     dataset,
@@ -89,16 +89,19 @@ def split_dataset(
     return train_set, val_set
 
 
-@hydra.main(config_path=str(Directories.CONFIG_PATH / "hydra_configs"),config_name='config.yaml')
+@hydra.main(
+    config_path=str(Directories.CONFIG_PATH / "hydra_configs"),
+    config_name="config.yaml",
+)
 def main(config):
-
-
+    
     if config.wandb:
         wandb.login()
-    model_name = get_model_name(config, sys_args = sys.argv)
-    dataset = load_data(model=model_name, dataset=config.dataset)
-    kfold= KFold(n_splits=config.n_splits, shuffle=True, random_state=config.seed)
     
+    model_name = get_model_name(config, sys_args=sys.argv)
+    dataset = load_data(model=model_name, dataset=config.dataset)
+    kfold = KFold(n_splits=config.n_splits, shuffle=True, random_state=config.seed)
+
     if config.remove_old_checkpoints:
         shutil.rmtree(Directories.CHECKPOINT_PATH / model_name)
 
@@ -106,17 +109,26 @@ def main(config):
         loggers = []
         if config.wandb:
             reset_wandb_env()
-            wandb.init(group = config.run_name,name=f"fold_{k}", config=dict(config))
-            loggers.append(WandbLogger()) 
+            wandb.init(group=config.run_name, name=f"fold_{k}", config=dict(config))
+            loggers.append(WandbLogger())
         call_backs = []
 
-        train_set, test_set = split_dataset(dataset, split_method="custom", split_idx=(train_idx, test_idx))
-        train_set, val_set = train_val_split(train_set, test_size=0.1, random_state=config.seed)
-        train_loader, val_loader, test_loader = get_dataloaders([train_set, val_set, test_set], batch_size=config.batch_size)
-        
-        checkpoint_callback = ModelCheckpoint(dirpath=get_checkpoint_path(model_name,k),**config.checkpoint_callback)
+        train_set, test_set = split_dataset(
+            dataset, split_method="custom", split_idx=(train_idx, test_idx)
+        )
+        train_set, val_set = train_val_split(
+            train_set, test_size=0.1, random_state=config.seed
+        )
+        train_loader, val_loader, test_loader = get_dataloaders(
+            [train_set, val_set, test_set], batch_size=config.batch_size
+        )
+
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=get_checkpoint_path(model_name, k), **config.checkpoint_callback
+        )
+
         call_backs.append(checkpoint_callback)
-        
+
         model = init_model(model=model_name, model_kwargs=config.model)
 
         trainer = Trainer(
@@ -124,13 +136,14 @@ def main(config):
             callbacks=call_backs,
             **config.trainer,
         )
-    
-        trainer.fit(model,train_dataloaders=train_loader,val_dataloaders=val_loader)
-        trainer.test(model,dataloaders=test_loader,ckpt_path=checkpoint_callback.best_model_path)
+
+        trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        trainer.test(
+            model,
+            dataloaders=test_loader,
+            ckpt_path=checkpoint_callback.best_model_path,
+        )
         wandb.finish()
-
-
-
 
 
 if __name__ == "__main__":
