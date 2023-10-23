e39f324670c3cbf22f0cc0b828d9434cddfee874
Villads Tristan Stokbro <57710093+vstokbro@users.noreply.github.com>
2023-10-23 10:46:44 +0200

    Update definitions.py

--- graph_package/configs/definitions.py
+++ graph_package/configs/definitions.py
@@ -1,8 +1,6 @@
 from graph_package.src.models import DeepDDS, RESCAL
 from graph_package.src.pl_modules import DeepDDS_PL, Rescal_PL
-from graph_package.src.pl_modules import KnowledgeGraphCompletion
 from graph_package.src.etl.dataloaders import ONEIL_DeepDDS, ONEIL_RESCAL
-from torchdrug.datasets import WN18RR
 
 model_dict = {"deepdds": DeepDDS_PL, "rescal": Rescal_PL}
 dataset_dict = {
