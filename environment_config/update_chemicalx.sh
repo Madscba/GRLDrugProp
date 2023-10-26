c21d4973a10b3f66b01d1735838b4c830c985626
Villads Stokbro <s175548@student.dtu.dk>
2023-10-26 17:28:06 +0200

    final changes to hpc env

--- environment_config/update_chemicalx.sh
+++ environment_config/update_chemicalx.sh
@@ -1,4 +1,4 @@
-cx_constant_path=$(python ./environment_config/update_chemicalx.py)
+export cx_constant_path='.venv/lib/python3.9/site-packages/chemicalx/constants.py'
 rm -rf $cx_constant_path
 parentdir="$(dirname "$cx_constant_path")"
 cp ./environment_config/constants.py $parentdir
