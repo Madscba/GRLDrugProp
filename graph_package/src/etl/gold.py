9a06b61ef281993633a1e234f59089654ee422c3
s175548 <s175548@student.dtu.dk>
2023-11-03 11:51:20 +0100

    fixed rescal

--- graph_package/src/etl/gold.py
+++ graph_package/src/etl/gold.py
@@ -21,6 +21,7 @@ def load_drug_info_drugcomb():
         drug_dict = json.load(f)
     return drug_dict
 
+
 def load_cell_info_drugcomb():
     data_path = Directories.DATA_PATH / "bronze" / "drugcomb" / "cell_line_dict.json"
     with open(data_path) as f:
@@ -28,10 +29,9 @@ def load_cell_info_drugcomb():
     return cell_line_dict
 
 
-
 def agg_loewe_and_make_binary(df: pd.DataFrame):
     sub_df = (
-        df.groupby(["drug_row", "drug_col", "cell_line_name"])
+        df.groupby(["drug_1_name", "drug_2_name", "cell_line_name"])
         .mean()
         .reset_index()
     )
@@ -45,18 +45,18 @@ def agg_loewe_and_make_binary(df: pd.DataFrame):
 
 def create_drug_id_vocabs(df: pd.DataFrame):
     # Create unique drug ID's
-    unique_drugs = set(df["drug_row"]).union(set(df["drug_col"]))
+    unique_drugs = set(df["drug_1_name"]).union(set(df["drug_2_name"]))
     drug_id_mapping = {drug: idx for idx, drug in enumerate(unique_drugs)}
-    df["drug_row_id"] = df["drug_row"].map(drug_id_mapping)
-    df["drug_col_id"] = df["drug_col"].map(drug_id_mapping)
+    df["drug_1_id"] = df["drug_1_name"].map(drug_id_mapping)
+    df["drug_2_id"] = df["drug_2_name"].map(drug_id_mapping)
 
     return df, drug_id_mapping
 
 
 def create_cell_line_id_vocabs(df: pd.DataFrame):
     # Create unique cell-line ID's based on context and label
-    sub_df = df.loc[df["label"] == 1]
-    sub_df.loc[:,["context_id"]] = sub_df.groupby(["context", "label"]).ngroup()
+    sub_df = df.copy()
+    sub_df.loc[:, ["context_id"]] = df.groupby(["context", "label"]).ngroup()
     sub_df.drop_duplicates(subset=["context", "context_id"], keep="first", inplace=True)
 
     # Create vocab to map cell line name to graph cell line ID
@@ -66,16 +66,17 @@ def create_cell_line_id_vocabs(df: pd.DataFrame):
     cell_line_name_mapping = {
         idx: name for name, idx in sub_df.loc[:, ["context", "context_id"]].values
     }
-    
+
     df["context_id"] = df["context"].map(cell_line_mapping)
     return df, cell_line_name_mapping
 
 
-def save_vocabs(drug_vocab, cell_line_vocab):
+def save_vocabs(drug_vocab, cell_line_vocab, save_suffix=""):
     save_path = Directories.DATA_PATH / "gold" / "oneil"
     save_path.mkdir(parents=True, exist_ok=True)
     for vocab, name in zip(
-        (drug_vocab, cell_line_vocab), ["entity_vocab.json", "relation_vocab.json"]
+        (drug_vocab, cell_line_vocab),
+        [f"entity_vocab{save_suffix}.json", f"relation_vocab{save_suffix}.json"],
     ):
         with open(save_path / name, "w") as json_file:
             json.dump(vocab, json_file)
@@ -83,7 +84,7 @@ def save_vocabs(drug_vocab, cell_line_vocab):
 
 def download_drug_dict_deepdds_original():
     url = "https://raw.githubusercontent.com/Sinwang404/DeepDDs/master/data/drugs_0_10.csv"
-    save_path = Directories.DATA_PATH / "gold" / "oneil" 
+    save_path = Directories.DATA_PATH / "gold" / "oneil"
     save_path.mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     with open(save_path / "drug_dict.json", "wb") as f:
@@ -93,8 +94,11 @@ def download_drug_dict_deepdds_original():
 
 
 def get_drug_dict_deepdds_original():
-    drug_name = pd.read_csv("https://raw.githubusercontent.com/Sinwang404/DeepDDs/master/data/drugs_0_10.csv",header=None)
-    drug_names_dict = {val[0]:val[1] for val in drug_name.values}
+    drug_name = pd.read_csv(
+        "https://raw.githubusercontent.com/Sinwang404/DeepDDs/master/data/drugs_0_10.csv",
+        header=None,
+    )
+    drug_names_dict = {val[0]: val[1] for val in drug_name.values}
     drug_names_list = drug_names_dict.keys()
     drug_dict = load_drug_info_drugcomb()
     deepdds_drug_dict = {}
@@ -102,14 +106,15 @@ def get_drug_dict_deepdds_original():
         synonyms = val["synonyms"].split("; ")
         for synonym in synonyms:
             for drug in drug_names_list:
-                synonym  = synonym.lower()
-                if  (drug.lower() == synonym) or (drug.lower() in synonym.split(" ")):
+                synonym = synonym.lower()
+                if (drug.lower() == synonym) or (drug.lower() in synonym.split(" ")):
                     assert drug not in deepdds_drug_dict
                     deepdds_drug_dict[drug_names_dict[drug]] = val["dname"]
                     del drug_names_dict[drug]
                     break
     assert len(drug_names_list) == 0
     return deepdds_drug_dict
+
 
 def get_cell_line_dict_deepdds_original(df: pd.DataFrame):
     cell_line_dict = load_cell_info_drugcomb()
@@ -119,8 +124,10 @@ def get_cell_line_dict_deepdds_original(df: pd.DataFrame):
         synonyms = val["synonyms"].split("; ")
         for synonym in synonyms:
             for cell_line in cell_line_names:
-                synonym  = synonym.lower()
-                if  (cell_line.lower() == synonym) or (cell_line.lower() in synonym.split(" ")):
+                synonym = synonym.lower()
+                if (cell_line.lower() == synonym) or (
+                    cell_line.lower() in synonym.split(" ")
+                ):
                     assert cell_line not in deepdds_cell_dict
                     deepdds_cell_dict[cell_line] = val["name"]
                     cell_line_names.remove(cell_line)
@@ -129,12 +136,11 @@ def get_cell_line_dict_deepdds_original(df: pd.DataFrame):
     return deepdds_cell_dict
 
 
-
-def get_drug_cell_line_ids(df: pd.DataFrame):    
+def get_drug_cell_line_ids(df: pd.DataFrame):
     load_path = Directories.DATA_PATH / "gold" / "oneil"
     entity_vocab = json.load(open(load_path / "entity_vocab.json"))
     relation_vocab = json.load(open(load_path / "relation_vocab.json"))
-    relation_vocab = {v:k for k,v in relation_vocab.items()}
+    relation_vocab = {v: k for k, v in relation_vocab.items()}
     df["drug_1_id"] = df["drug_1_name"].map(entity_vocab)
     df["drug_2_id"] = df["drug_2_name"].map(entity_vocab)
     set(df["cell"]).difference(set(relation_vocab.keys()))
@@ -143,27 +149,41 @@ def get_drug_cell_line_ids(df: pd.DataFrame):
 
 
 def make_original_deepdds_dataset():
-    # Download triplets from DeepDDS repo 
-    df = pd.read_csv("https://raw.githubusercontent.com/Sinwang404/DeepDDs/master/data/new_labels_0_10.csv")
+    # Download triplets from DeepDDS repo
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/Sinwang404/DeepDDs/master/data/new_labels_0_10.csv"
+    )
     drug_dict_deepdds = get_drug_dict_deepdds_original()
     cell_line_dict_deepdds = get_cell_line_dict_deepdds_original(df)
+
     df["drug_1_name"] = df["drug1"].map(drug_dict_deepdds)
     df["drug_2_name"] = df["drug2"].map(drug_dict_deepdds)
     df["context"] = df["cell"].map(cell_line_dict_deepdds)
-    df = get_drug_cell_line_ids(df)
-    df.drop(["drug1","drug2","cell"],axis=1,inplace=True)
-    df =  df[["drug_1_name","drug_2_name","label","context","drug_1_id","drug_2_id","context_id"]]
-    df.to_csv(Directories.DATA_PATH / "gold" / "oneil" / "deepdds_original.csv", index=False)
+
+    df, drug_vocab = create_drug_id_vocabs(df)
+    df, cell_line_vocab = create_cell_line_id_vocabs(df)
+    save_vocabs(drug_vocab, cell_line_vocab, save_suffix="_deepdds_original")
+    df = df[
+        [
+            "drug_1_name",
+            "drug_2_name",
+            "label",
+            "context",
+            "drug_1_id",
+            "drug_2_id",
+            "context_id",
+        ]
+    ]
+    assert df.isna().sum().sum() == 0
+    df.to_csv(
+        Directories.DATA_PATH / "gold" / "oneil" / "deepdds_original.csv", index=False
+    )
 
 
 def make_triplets_oneil():
     save_path = Directories.DATA_PATH / "gold" / "oneil"
     save_path.mkdir(parents=True, exist_ok=True)
-
     df = load_oneil()
-    df, drug_vocab = create_drug_id_vocabs(df)
-    df = agg_loewe_and_make_binary(df)
-
     rename_dict = {
         "drug_row": "drug_1_name",
         "drug_col": "drug_2_name",
@@ -173,14 +193,17 @@ def make_triplets_oneil():
         "drug_col_id": "drug_2_id",
     }
     df.rename(columns=rename_dict, inplace=True)
-    df = df[rename_dict.values()]
 
+    df, drug_vocab = create_drug_id_vocabs(df)
     df, cell_line_vocab = create_cell_line_id_vocabs(df)
+    df = agg_loewe_and_make_binary(df)
+
+    df = df[rename_dict.values()]
 
     save_vocabs(drug_vocab, cell_line_vocab)
     df.to_csv(save_path / "oneil.csv", index=False)
 
 
 if __name__ == "__main__":
-    #make_triplets_oneil()
+    # make_triplets_oneil()
     make_original_deepdds_dataset()
