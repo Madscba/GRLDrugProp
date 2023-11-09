ee78ec8e41a2859b69de7ebea88238b60564ad4e
Madscba <Madscba@gmail.com>
2023-11-09 08:18:25 +0100

    refactored the two pca_2d functions into 1

--- graph_package/src/explainability/pca.py
+++ graph_package/src/explainability/pca.py
@@ -19,7 +19,7 @@ def save_rescal_embedding(config, model):
     print("dumped file at", emb_save_path)
 
 
-def pca_2d(X):
+def pca_2d(X, k_means=None, k_means_clusters=4):
     # 2D PCA
     pca = PCA(n_components=2)
     pca.fit(X)
@@ -27,6 +27,47 @@ def pca_2d(X):
     fig_2d = plt.figure(figsize=(8, 6))
     ax_2d = fig_2d.add_subplot(111)
     ax_2d.scatter(X_reduced_2d[:, 0], X_reduced_2d[:, 1])
+
+    if k_means is not None:
+        reduced_data = PCA(n_components=2).fit_transform(X)
+        kmeans = KMeans(init="k-means++", n_clusters=k_means_clusters, n_init=4)
+        kmeans.fit(reduced_data)
+        h = 0.02
+        x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+        y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        plt.figure(1)
+        plt.clf()
+        plt.imshow(
+            Z,
+            interpolation="nearest",
+            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+            cmap=plt.cm.Paired,
+            aspect="auto",
+            origin="lower",
+        )
+        plt.plot(reduced_data[:, 0], reduced_data[:, 1], "k.", markersize=2)
+        centroids = kmeans.cluster_centers_
+        plt.scatter(
+            centroids[:, 0],
+            centroids[:, 1],
+            marker="x",
+            s=169,
+            linewidths=3,
+            color="w",
+            zorder=10,
+        )
+        plt.title(
+            "K-means clustering on the data (PCA-reduced data)\n"
+            "Centroids are marked with white cross"
+        )
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
+        plt.xticks(())
+        plt.yticks(())
+
     ax_2d.set_title("2D Scatter Plot")
     plt.show()
 
@@ -56,58 +97,10 @@ def pca_3d(X):
     plt.show()
 
 
-def pca_2d_w_kmeans(X, k_means=4):
-    # third
-    reduced_data = PCA(n_components=2).fit_transform(X)
-    kmeans = KMeans(init="k-means++", n_clusters=k_means, n_init=4)
-    kmeans.fit(reduced_data)
-    # Step size of the mesh. Decrease to increase the quality of the VQ.
-    h = 0.02  # point in the mesh [x_min, x_max]x[y_min, y_max].
-    # Plot the decision boundary. For that, we will assign a color to each
-    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    # Obtain labels for each point in mesh. Use last trained model.
-    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.figure(1)
-    plt.clf()
-    plt.imshow(
-        Z,
-        interpolation="nearest",
-        extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-        cmap=plt.cm.Paired,
-        aspect="auto",
-        origin="lower",
-    )
-    plt.plot(reduced_data[:, 0], reduced_data[:, 1], "k.", markersize=2)
-    # Plot the centroids as a white X
-    centroids = kmeans.cluster_centers_
-    plt.scatter(
-        centroids[:, 0],
-        centroids[:, 1],
-        marker="x",
-        s=169,
-        linewidths=3,
-        color="w",
-        zorder=10,
-    )
-    plt.title(
-        "K-means clustering on the digits dataset (PCA-reduced data)\n"
-        "Centroids are marked with white cross"
-    )
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
-
-
 emb_save_path = r"C:\Users\Mads-\Documents\Universitet\Kandidat\5_semester\thesis\GRLDrugProp\data\embeddings\rescal_100.csv"
 
 X = pd.read_csv(emb_save_path)
 k_means = 4
 pca_2d(X)
+pca_2d(X, k_means=True)
 pca_3d(X)
-pca_2d_w_kmeans(X, k_means)
