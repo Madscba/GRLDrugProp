b61fcf20972c80fd8a456d3b750560cc986e7730
Madscba <Madscba@gmail.com>
2023-12-22 13:16:11 +0100

    started on mono response to GNN MLP prediction head

--- graph_package/src/models/gnn/prediction_head.py
+++ graph_package/src/models/gnn/prediction_head.py
@@ -2,12 +2,15 @@ from torch import nn
 import torch
 import json
 from graph_package.configs.directories import Directories
+import pandas as pd
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class MLP_PredictionHead(nn.Module):
-    def __init_(self, drug_input_dim):
+    def __init_(self, drug_input_dim, use_mono_response: bool = False):
         super(MLP_PredictionHead, self).__init__()
+        self.use_mono_response = use_mono_response
         self.ccle = self._load_ccle()
         cell_line_input_dim = self.ccle.shape[1]
         self.cell_line_mlp = nn.Sequential(
@@ -17,35 +20,69 @@ class MLP_PredictionHead(nn.Module):
             nn.ReLU(),
         )
 
+        if self.use_mono_response:
+            self.mono_r = self._load_mono_response()
+            global_mlp_input_dim = 2 * drug_input_dim + 64 + 3 * 2
+        else:
+            global_mlp_input_dim = 2 * drug_input_dim + 64
+
         self.global_mlp = nn.Sequential(
-            nn.Linear(2*drug_input_dim+64, 256),
+            nn.Linear(global_mlp_input_dim, 256),
             nn.ReLU(),
             nn.Linear(256, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Linear(64,1)
+            nn.Linear(64, 1),
         )
-    def _load_ccle(self):
-        feature_path = Directories.DATA_PATH / "features" / "cell_line_features" / "CCLE_954_gene_express.json"
-        with open(feature_path) as f:
-            all_edge_features = json.load(f)
-        vocab_path = Directories.DATA_PATH / "gold" / "oneil_almanac" / "relation_vocab.json"
+
+    def _load_mono_response(self):
+        gold_oneil_alm = Directories.DATA_PATH / "gold" / "oneil_almanac"
+        mono_response_path = path = gold_oneil_alm / "mono_response.csv"
+        mono_response = pd.read_csv(mono_response_path)
+        vocab_path = gold_oneil_alm / "entity_vocab.json"
         with open(vocab_path) as f:
-            relation_vocab = json.load(f)
-        vocab_reverse = {v:k for k,v in relation_vocab.items()}
+            entity_vocab = json.load(f)
+        vocab_reverse = {v: k for k, v in entity_vocab.items()}
         ids = sorted(list(vocab_reverse.keys()))
-        ccle = torch.tensor([all_edge_features[vocab_reverse[id]] for id in ids], device=device)
+        ccle = torch.tensor(
+            [all_edge_features[vocab_reverse[id]] for id in ids], device=device
+        )
         return ccle
 
-    def forward(self, d1_embd: torch.Tensor, d2_embd: torch.Tensor, context_ids: torch.Tensor) -> torch.FloatTensor:
+    def _load_ccle(self):
+        feature_path = (
+            Directories.DATA_PATH
+            / "features"
+            / "cell_line_features"
+            / "CCLE_954_gene_express.json"
+        )
+        with open(feature_path) as f:
+            all_edge_features = json.load(f)
+        vocab_path = (
+            Directories.DATA_PATH / "gold" / "oneil_almanac" / "relation_vocab.json"
+        )
+        with open(vocab_path) as f:
+            relation_vocab = json.load(f)
+        vocab_reverse = {v: k for k, v in relation_vocab.items()}
+        ids = sorted(list(vocab_reverse.keys()))
+        ccle = torch.tensor(
+            [all_edge_features[vocab_reverse[id]] for id in ids], device=device
+        )
+        return ccle
+
+    def forward(
+        self, d1_embd: torch.Tensor, d2_embd: torch.Tensor, context_ids: torch.Tensor
+    ) -> torch.FloatTensor:
         c = self.cell_line_mlp(self.ccle[context_ids])
+        if self.use_mono_response:
+            pass
         input = torch.concat([d1_embd, d2_embd, c], dim=1)
         output = self.global_mlp(input)
         return output
 
 
-class DistMult():
+class DistMult:
     """
     DistMult embedding proposed in `Embedding Entities and Relations for Learning and Inference in Knowledge Bases`_.
 
@@ -59,18 +96,8 @@ class DistMult():
         l3_regularization (float, optional): weight for l3 regularization
     """
 
-    def __init__(
-        self,
-        ent_tot,
-        rel_tot, 
-        dim,
-        max_score=30
-    ):
-        super().__init__(
-            num_entity=ent_tot, 
-            num_relation=rel_tot, 
-            embedding_dim=dim
-        )
+    def __init__(self, ent_tot, rel_tot, dim, max_score=30):
+        super().__init__(num_entity=ent_tot, num_relation=rel_tot, embedding_dim=dim)
         self.num_entity = ent_tot
         self.num_relation = rel_tot
 
@@ -97,15 +124,14 @@ class DistMult():
         r = self.relation[r_index]
         score = (h * r * t).sum(dim=-1)
         return score
-    
+
     def forward(self, d1_emb, d2_emb, context_ids):
         score = self.score_triplet(context_ids, d1_emb, d2_emb)
-        return score 
-    
+        return score
+
     def predict(self, data):
         score = self.forward(data)
         return score.cpu().data.cpu().numpy()
-    
+
     def __name__(self) -> str:
         return "DistMult"
-    
