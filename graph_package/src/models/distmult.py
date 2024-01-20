9b1a1d54eb2bcbf2328bf3cb728e04e6f33a2a4d
Villads Tristan Stokbro <57710093+vstokbro@users.noreply.github.com>
2024-01-20 10:32:08 +0100

    Update distmult.py

--- graph_package/src/models/distmult.py
+++ graph_package/src/models/distmult.py
@@ -1,6 +1,8 @@
 from torch import nn
 from torchdrug.models import DistMult as distmult
 import torch.nn.functional as F
+import functools
+
 class DistMult(distmult):
     """
     DistMult embedding proposed in `Embedding Entities and Relations for Learning and Inference in Knowledge Bases`_.
@@ -21,7 +23,7 @@ class DistMult(distmult):
         rel_tot, 
         dim,
         max_score=30,
-        feature_dropout: float = 0.0,
+        feature_dropout: float = 0.1,
     ):
         super().__init__(
             num_entity=ent_tot, 
@@ -29,7 +31,9 @@ class DistMult(distmult):
             embedding_dim=dim
         )
         self.max_score = max_score
-        self.feature_dropout_p = feature_dropout
+        self.feature_dropout = (
+            functools.partial(F.dropout, p=feature_dropout) if feature_dropout else None
+        )
         nn.init.xavier_uniform_(self.entity)
         nn.init.xavier_uniform_(self.relation)
 
@@ -50,8 +54,8 @@ class DistMult(distmult):
         h = self.entity[h_index]
         r = self.relation[r_index]
         t = self.entity[t_index]
-        if self.feature_dropout_p:
-            h, r, t = F.dropout(h,p=self.feature_dropout_p), F.dropout(r,p=self.feature_dropout_p),F.dropout(t,p=self.feature_dropout_p)
+        if self.feature_dropout:
+            h, r, t = [self.feature_dropout(x) for x in (h,r,t)]
         score = (h * r * t).sum(dim=-1)
         return score
     
