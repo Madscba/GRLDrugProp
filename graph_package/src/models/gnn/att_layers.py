97502b2e5e87e75cfa52aa84acc0bae384f73c50
s175549 <46479297+s175549@users.noreply.github.com>
2024-01-23 10:25:30 +0100

    Updated rgat config and input

--- graph_package/src/models/gnn/att_layers.py
+++ graph_package/src/models/gnn/att_layers.py
@@ -360,7 +360,7 @@ class GraphAttentionLayer(MessagePassingBase):
         dataset,
         n_heads: int = 4,
         negative_slope: int = 0.2,
-        dropout: int = 0.6,
+        feature_dropout: int = 0.1,
         concat_hidden=True,
         batch_norm=False,
     ):
@@ -368,7 +368,7 @@ class GraphAttentionLayer(MessagePassingBase):
         self.input_dim = input_dim
         self.num_relation = num_relation
         self.dataset = dataset
-        self.dropout = dropout
+        self.dropout = feature_dropout
         self.concat_hidden = concat_hidden
         self.negative_slope = negative_slope
         self.n_heads = n_heads
